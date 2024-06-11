546b9cac854a2471575c78eb49fd2bdbca8fda1f
rajat0208 <rajathsrestha0208@gmail.com>
2024-06-11 21:04:39 +0545

    feat: implemented copyright section

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,11 +4,22 @@ import Logo from "../assets/logo.png"
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { FaLinkedin } from "react-icons/fa";
 import { BsYoutube } from "react-icons/bs";
+import CC from "../assets/ccLogo.png"
+import { FaArrowUp } from "react-icons/fa6";
+
 
 export default function Footer() {
+    console.log('Scrolling to top');
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
     return (
         <div className='footer'>
             <div className='footer-group'>
+                <div className='footer-overlay'></div>
                 <div className='footer-wrapper'>
                     <div className='footer-company'>
                         <img src={Logo} className='footer-logo'></img>
@@ -32,24 +43,31 @@ export default function Footer() {
                             </ul>
                         </div>
                         <div className='footer-section footerresources'>
-                        <h3>Resources</h3>
-                        <ul>
-                            <li><a href="#transparency-toolkit">Transparency Toolkit</a></li>
-                            <li><a href="#evidence">Evidence</a></li>
-                            <li><a href="#best-practices">Best Practices</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer-section work'>
-                        <h3>Work with Us</h3>
-                        <ul>
-                            <li><a href="#contact-us">Contact Us</a></li>
-                            <li><a href="#join-us">Join Us</a></li>
-                        </ul>
-                    </div>
+                            <h3>Resources</h3>
+                            <ul>
+                                <li><a href="#transparency-toolkit">Transparency Toolkit</a></li>
+                                <li><a href="#evidence">Evidence</a></li>
+                                <li><a href="#best-practices">Best Practices</a></li>
+                            </ul>
+                        </div>
+                        <div className='footer-section work'>
+                            <h3>Work with Us</h3>
+                            <ul>
+                                <li><a href="#contact-us">Contact Us</a></li>
+                                <li><a href="#join-us">Join Us</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className='copyright'>
+            </div>
 
+            <div className='copyright'>
+                <div className='copyright-wrapper'>
+                    <p>Terms and Conditions</p>
+                    <img src={CC}></img>
+                </div>
+                <div className='scroll-to-top' onClick={scrollToTop}>
+                    <FaArrowUp color='grey' />
                 </div>
             </div>
         </div>
--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,89 +1,100 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import axios from 'axios'
+
 import "../App.css"
 import { FaArrowRight } from "react-icons/fa6";
-import News1 from "../assets/news1.jpeg";
-import News2 from "../assets/news2.jpeg";
-import News3 from "../assets/news3.jpeg";
-import News4 from "../assets/news4.jpeg";
-import News5 from "../assets/news5.jpeg";
-import News6 from "../assets/news6.jpeg";
+// import News1 from "../assets/news1.jpeg";
+// import News2 from "../assets/news2.jpeg";
+// import News3 from "../assets/news3.jpeg";
+// import News4 from "../assets/news4.jpeg";
+// import News5 from "../assets/news5.jpeg";
+// import News6 from "../assets/news6.jpeg";
 
 
 export default function News() {
+
+    const [news,setNews]=useState([]);
+    const [loading, setLoading] = useState(true);
+    const [displayedNewsCount, setDisplayedNewsCount] = useState(6);
+    const [showAllNews, setShowAllNews] = useState(false);
+    const newsRef = useRef(null);
+
+    useEffect(()=>{
+        axios.get("https://raw.githubusercontent.com/younginnovations/internship-challenges/master/front-end/news_list.json")
+        .then(response =>{
+            console.log('Fetched data:', response.data);
+            setNews(response.data.news || [])
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('There was an error fetching the news!', error);
+            setLoading(false);
+        });
+    },[]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (newsRef.current) {
+                const { scrollHeight, clientHeight, scrollTop } = newsRef.current;
+                const scrollOffset = 200; 
+                if (scrollHeight - scrollTop <= clientHeight + scrollOffset) {
+                    setShowAllNews(true);
+                }
+            }
+        };
+
+        if (newsRef.current) {
+            newsRef.current.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            if (newsRef.current) {
+                newsRef.current.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
+
+    const handleViewAllNews = () => {
+        setDisplayedNewsCount(news.length);
+        setShowAllNews(true);
+    };
+
+    if (loading) {
+        return <p>Loading news...</p>;
+    }
+
+
     return (
         <div className='news'>
             <div className='wrapper'>
                 <h2>News</h2>
                 <p> We aim to stop public funds siphoning off and we have a plan that will help. We are working at the national and EU levels to advance.</p>
-                <div className='item'>
-                    <div className='row'>
-                        <div className='newsCard'>
-                            <div className='newsContainer'>
-                                <div className='newsImage'>
-                                    <img src={News1}></img>
+                <div className='item' ref={newsRef}>
+                {news.slice(0, displayedNewsCount).map((article, index) =>  (
+                            <div className='row' key={index}>
+                                <div className='newsCard'>
+                                    <div className='newsContainer'>
+                                        <div className='newsImage'>
+                                            <img src={article.image} alt={article.title} />
+                                        </div>
+                                        <h4>{article.title}</h4>
+                                        <p>{article.content.slice(0, 100)}...</p>
+                                        <a href='#' className='news-learn-more'>
+                                            Learn More<FaArrowRight />
+                                        </a>
+                                    </div>
                                 </div>
-                                <h4>Statement from the Catalogue Coalition...</h4>
-                                <p>In view of Russia’s military action against the Ukrainian people, being concerned about ensuring that European funds...</p>
-                                <a href='#' className='news-learn-more'>Learn More<FaArrowRight /></a>
                             </div>
-                        </div>
-                        <div className='newsCard'>
-                        <div className='newsContainer'>
-                                <div className='newsImage'>
-                                    <img src={News2}></img>
-                                </div>
-                                <h4>Statement from the Catalogue Coalition...</h4>
-                                <p>In view of Russia’s military action against the Ukrainian people, being concerned about ensuring that European funds...</p>
-                                <a href='#' className='news-learn-more'>Learn More<FaArrowRight /></a>
-                            </div>
-                        </div>
-                        <div className='newsCard'>
-                        <div className='newsContainer'>
-                                <div className='newsImage'>
-                                    <img src={News3}></img>
-                                </div>
-                                <h4>Statement from the Catalogue Coalition...</h4>
-                                <p>In view of Russia’s military action against the Ukrainian people, being concerned about ensuring that European funds...</p>
-                                <a href='#' className='news-learn-more'>Learn More<FaArrowRight /></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='newsCard'>
-                        <div className='newsContainer'>
-                                <div className='newsImage'>
-                                    <img src={News4}></img>
-                                </div>
-                                <h4>Statement from the Catalogue Coalition...</h4>
-                                <p>In view of Russia’s military action against the Ukrainian people, being concerned about ensuring that European funds...</p>
-                                <a href='#' className='news-learn-more'>Learn More<FaArrowRight /></a>
-                            </div>
-                        </div>
-                        <div className='newsCard'>
-                        <div className='newsContainer'>
-                                <div className='newsImage'>
-                                    <img src={News5}></img>
-                                </div>
-                                <h4>Statement from the Catalogue Coalition...</h4>
-                                <p>In view of Russia’s military action against the Ukrainian people, being concerned about ensuring that European funds...</p>
-                                <a href='#' className='news-learn-more'>Learn More<FaArrowRight /></a>
-                            </div>
-                        </div>
-                        <div className='newsCard'>
-                        <div className='newsContainer'>
-                                <div className='newsImage'>
-                                    <img src={News6}></img>
-                                </div>
-                                <h4>Statement from the Catalogue Coalition...</h4>
-                                <p>In view of Russia’s military action against the Ukrainian people, being concerned about ensuring that European funds...</p>
-                                <a href='#' className='news-learn-more'>Learn More<FaArrowRight /></a>
-                            </div>
-                        </div>
-                    </div>
+                        ))
+                      (
+                        <p>No news article available</p>
+                    )}
                 </div>
-                <div className='viewAllNews'>
-                    <p>View all News</p>
-                </div>
+                {displayedNewsCount < news.length && (
+                    <div className='viewAllNews' onClick={handleViewAllNews}>
+                        <p>View all News</p>
+                    </div>
+                )}
             </div>
         </div>
     )
